217786d01506d4e422b34bf81c0b1f3d25c2afd3
rmarsack <rmarsack@gmail.com>
2016-12-27 08:37:09 -0500

    Lecture 94: Adding links to navigate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
 import { DropdownDirective } from './dropdown.directive'
 import { RecipeService } from "./recipes";
 import { ShoppingListService } from "./shopping-list";
+import { routing } from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { ShoppingListService } from "./shopping-list";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/recipes/', pathMatch: 'full' },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent },
     { path: 'shopping-list', component: ShoppingListComponent },
 ];
