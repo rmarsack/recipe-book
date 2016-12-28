1c711fb178ccd4c3dae131916ab23cb6fae432fa
rmarsack <rmarsack@gmail.com>
2016-12-28 17:30:03 -0500

    Lecture 99: Wiring up "Edit" and "Delete"

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -22,4 +22,9 @@ export class RecipeService {
   getRecipe(id:number) {
     return this.recipes[id];
   }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipes.splice(this.recipes.indexOf(recipe), 1)
+
+  }
 }
