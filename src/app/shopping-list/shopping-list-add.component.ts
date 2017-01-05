d62a6e358a15d9deeb7745ba9a41a7cb367a8db5
rmarsack <rmarsack@gmail.com>
2017-01-05 07:11:41 -0500

    132: Adding and Editing mode

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true;
 
   constructor() { }
 
