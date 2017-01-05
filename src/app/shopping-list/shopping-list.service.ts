6c3208cf30ab6ff0a9b7b8313427b43833845142
rmarsack <rmarsack@gmail.com>
2017-01-05 07:56:39 -0500

    136: Editing Items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -14,5 +14,8 @@ export class ShoppingListService {
   addItem(item: Ingredient){
     this.items.push(item);
   }
-
+  editItem(oldItem: Ingredient, newItem: Ingredient){
+    this.items[this.items.indexOf(oldItem)] = newItem;
+    
+  }
  }
