709da8ddfaec4330d928b9a1b46c49a1c9655923
rmarsack <rmarsack@gmail.com>
2017-01-10 19:39:56 -0500

    Working after fixed from 141

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
+
 import { Recipe } from "./recipe";
 import { Ingredient } from "../shared";
 
 @Injectable()
 export class RecipeService {
+
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-8-edited-6.jpg', [
+    new Recipe('Schnitzel', 'Very tasty', 'http://toriavey.com/images/2011/02/IMG_1544.jpg', [
       new Ingredient('French Fries', 2),
       new Ingredient('Pork Meat', 1)
     ]),
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
-  constructor() { }
 
+  constructor() {}
 
   getRecipes() {
     return this.recipes;
-
   }
 
-  getRecipe(id:number) {
+  getRecipe(id: number) {
     return this.recipes[id];
   }
 
   deleteRecipe(recipe: Recipe) {
-    this.recipes.splice(this.recipes.indexOf(recipe), 1)
-
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
   }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
 }
